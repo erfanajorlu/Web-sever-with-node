ba396b0c74c8129321321ec30e2b5656fb20a394
Erfan Ajorlu <erfanajoglu098@gmail.com>
2023-12-26 14:59:50 +0330

    8. Parameterized URLs

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
-const http = require('http');
-
+const http = require("http");
 
 const PORT = 3000;
 
+friends = [
+  {
+    id: 0,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 1,
+    name: "Nikola Tesla",
+  },
+];
 const server = http.createServer();
 
-server.on('request' , (req , res) => {
-    // res.writeHead(200 , {
-    //     'Content-Type' : 'application/json',
-    // });
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
 
-    if(req.url === '/friends'){
-        res.statusCode = 200;
-        res.setHeader = ('Content-Type' , 'application/json');
-        res.end(JSON.stringify({
-            id : 1,
-            name : 'Sir Isaac Newton'
-        })); 
-    }else if(req.url === '/messages'){
-        res.setHeader('Content_Type' , 'text/html');
-        res.write('<html>');
-        res.write('<body>');
-        res.write('<ul>');
-        res.write('<li>Hello Isaac!</li>');
-        res.write('<li>What are your thoughts on astronomy?</li>');
-        res.write('</ul>');
-        res.write('</body>');
-        res.write('</html>');
-        res.end();
-    }else{
+  if (items[1] === "friends") {
+    res.statusCode = 200;
+    res.setHeader = ("Content-Type", "application/json");
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      if (friendIndex < friends.length) {
+        res.end(JSON.stringify(friends[friendIndex]));
+      } else {
         res.statusCode = 404;
         res.end();
+      }
+    } else {
+      res.end(JSON.stringify(friends));
     }
-    
+  } else if (items[1] === "messages") {
+    res.setHeader("Content_Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac!</li>");
+    res.write("<li>What are your thoughts on astronomy?</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
-server.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`);
-}); 
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
