1d2a020b392470b30dfdd845424f7156de1de4a5
Erfan Ajorlu <erfanajoglu098@gmail.com>
2023-12-26 14:21:50 +0330

    change content-type

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const PORT = 3000;
 
 const server = http.createServer((req , res) => {
     res.writeHead(200 , {
-        'Content-Type' : 'text/plain',
+        'Content-Type' : 'application/json',
     });
-    res.end('Hello! Sir Isaac Newton is your friend!'); 
+    res.end(JSON.stringify({
+        id : 1,
+        name : 'Isaac Newton'
+    })); 
 })
 
 server.listen(PORT , ()=>{
